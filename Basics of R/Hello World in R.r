b86936785ab52d1a01333781975258a4505c3746
Binam Karki <58617203+binamify@users.noreply.github.com>
2021-10-24 21:34:47 +0545

    Update Hello World in R.r

--- Basics of R/Hello World in R.r
+++ Basics of R/Hello World in R.r
@@ -1,1 +1,9 @@
-
+# We can use the print() function
+print("Hello World!")
+#"Hello World!"
+# Quotes can be suppressed in the output
+print("Hello World!", quote = FALSE)
+#Hello World!
+ # If there are more than 1 item, we can concatenate using paste()
+>print(paste("How","are","you?"))
+#  "How are you?"
