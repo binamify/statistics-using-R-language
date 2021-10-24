9a4b1bf20689c76d7a403aaa5dffa20642b62692
Binam Karki <58617203+binamify@users.noreply.github.com>
2021-10-24 21:37:00 +0545

    Update Taking Input form User.r

--- Basics of R/Taking Input form User.r
+++ Basics of R/Taking Input form User.r
@@ -1,1 +1,12 @@
+my.name <- readline(prompt="Enter name: ")
+my.age <- readline(prompt="Enter age: ")
+# converting character into integer
+my.age <- as.integer(my.age)
+print(paste("Hi,", my.name, "next year you will be", my.age+1, "years old."))
 
+
+
+#output
+#Enter name: Mary
+#Enter age: 17
+# "Hi, Mary next year you will be 18 years old."
