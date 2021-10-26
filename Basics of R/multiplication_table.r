c2dd744ec467ca1f297e6a256c07787ba888584c
Binam Karki <58617203+binamify@users.noreply.github.com>
2021-10-26 08:27:36 +0545

    Update multiplication_table.r

--- Basics of R/multiplication_table.r
+++ Basics of R/multiplication_table.r
@@ -1,1 +1,19 @@
+# R Program to find the multiplicationtable (from 1 to 10)
+# take input from the user
+num = 12
+# use for loop to iterate 10 times
+for(i in 1:10) {
+print(paste(num,'x', i, '=', num*i))
+}
 
+#Output
+# [1] "12 x 1 = 12"
+# [1] "12 x 2 = 24"
+# [1] "12 x 3 = 36"
+# [1] "12 x 4 = 48"
+# [1] "12 x 5 = 60"
+# [1] "12 x 6 = 72"
+# [1] "12 x 7 = 84"
+# [1] "12 x 8 = 96"
+# [1] "12 x 9 = 108"
+# [1] "12 x 10 = 120"
